00c3c875c5f841d438eba26019a16a447bf0b895
modimore <sirmodimore@gmail.com>
2015-04-25 17:33:02 -0400

    forgot to add some files 1 minute ago

--- grid.h
+++ grid.h
@@ -6,6 +6,34 @@
 #include "vbo_structs.h"
 #include "building.h"
 
+enum LOT_STATUS { L_EMPTY, L_FULL, L_ROAD, L_WATER, L_PARK, L_BLOCKED };
+
+class Lot { 
+private:
+  int x_index;
+  int z_index;
+  int status;
+  glm::vec4 color;
+  Building* here;
+public:
+  // constructor
+  Lot(int x, int z) 
+  : x_index(x), z_index(z), here(NULL) { status = 0; setColor(); }
+  
+  // accessors & modifiers
+  int get_x() const { return x_index; }
+  int get_z() const { return z_index; }
+  glm::vec4 getColor() const { return color; }
+  void setColor();
+  void setColor( const glm::vec4 & _color ) { color = _color; }
+  Building* getBuilding() const { return here; }
+  void setBuilding( Building* bldg ) { here = bldg; } 
+  
+  int getStatus() const { return status; }
+  void setStatus( int s ) { status = s; setColor(); }
+  bool isEmpty() const { return (here==NULL); }
+};
+
 /*
 class Lot { 
 private:
@@ -49,10 +77,11 @@ private:
   std::vector<bool> lotStatus;
   //std::vector<Block*> blocks;
   std::vector<Building> bldgs;
+  std::vector<Lot> lots;
   
   // grid geometry information
-  std::vector<VBOPosNormalColor> grid_tri_verts;
-  std::vector<VBOIndexedTri> grid_tri_indices;
+  std::vector<VBOPosNormalColor> lot_tri_verts;
+  std::vector<VBOIndexedTri> lot_tri_indices;
   std::vector<VBOPosNormalColor> grid_edge_verts;
   std::vector<VBOIndexedTri> grid_edge_indices;
   
@@ -61,14 +90,15 @@ private:
   std::vector<VBOIndexedTri> bldg_tri_indices;
   
   // VBOs
-  GLuint grid_tri_verts_VBO;
-  GLuint grid_tri_indices_VBO;
+  GLuint lot_tri_verts_VBO;
+  GLuint lot_tri_indices_VBO;
   GLuint grid_edge_verts_VBO;
   GLuint grid_edge_indices_VBO;
   GLuint bldg_tri_verts_VBO;
   GLuint bldg_tri_indices_VBO;
 private:
   int map2Dto1D( int i, int k ) const { return k*width + i; }
+  void map1Dto2D( int c, int& i, int& k ) const {i = c%width; k = c/width;}
 public:
   // grid setup functions
   Grid(ArgParser* _args);
@@ -77,8 +107,10 @@ public:
   
   // what?
   BoundingBox getBoundingBox() const { return bbox; }
-  bool getStatus(int i,int k) const { return lotStatus[map2Dto1D(i,k)]; }
-  void setStatus(int i,int k,bool val) { lotStatus[map2Dto1D(i,k)] = val; }
+  int getLotStatus(int i,int k) const { return lots[map2Dto1D(i,k)].getStatus(); }
+  void setLotStatus(int i,int k,const int& val) { lots[map2Dto1D(i,k)].setStatus(val); }
+  glm::vec4 getLotColor(int i,int k) const { return lots[map2Dto1D(i,k)].getColor(); }
+  void setLotColor(int i,int k,const glm::vec4& c) { lots[map2Dto1D(i,k)].setColor(c); }
   
   // Grid-filling Functions
   void fill(int i, int k, int bldg_index, int* r_val);
